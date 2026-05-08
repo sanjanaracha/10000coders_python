919520543573511e6121ac3cc66734007d056655
sanjanaracha <sanjanaracha16@gmail.com>
2026-05-08 12:57:20 +0530

    Add files via upload

---
+++ python_datatypes.py
@@ -0,0 +1,24 @@
+# 15. Basic data types in Python
+# int, float, str, bool
+
+# 16. Identify data type
+x = 10
+print(type(x))   # int
+
+# 17. Identify data type
+x = 10.5
+print(type(x))   # float
+
+# 18. Identify data type
+x = "Hello"
+print(type(x))   # str
+
+# 19. Identify data type
+x = True
+print(type(x))   # bool
+
+# 20. Examples
+a = 25
+b = 3.14
+c = "Hello"
+d = True
