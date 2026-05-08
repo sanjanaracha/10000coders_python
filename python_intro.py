a77e34416204b2ef5128b7589065a110946dc36b
sanjanaracha <sanjanaracha16@gmail.com>
2026-05-08 13:00:33 +0530

    Add files via upload

---
+++ python_intro.py
@@ -0,0 +1,62 @@
+# 1. What is Python?
+# Answer: Python is a high-level, interpreted, platform-independent, dynamically typed programming language.
+# It is easy to understand and learn, and it supports Object-Oriented Programming (OOP).
+
+# 2. Is Python case-sensitive?
+# Answer: Yes, Python is case-sensitive.
+# Example:
+name = "Sanjana"
+Name = "Python"
+
+# 3. What is the extension of a Python file?
+# Answer: .py
+
+# 4. Who created Python?
+# Answer: Guido van Rossum
+
+# 5. Is Python compiled or interpreted?
+# Answer: Python works in a hybrid way:
+# First, it is compiled into bytecode.
+# Then, it is interpreted by the Python Virtual Machine (PVM).
+
+# 6. What is a variable in Python?
+# Answer: A variable is a name used to store data in memory.
+x = 10
+
+# 7. Write a variable to store your name.
+name = "Sanjana"
+
+# 8. Can a variable name start with a number?
+# Answer: No
+
+# 9. Which is valid: my_name or my-name?
+# Answer: my_name is valid
+
+my_name = "Python"
+
+# 10. Is Age and age same variable?
+# Answer: No, they are different variables because Python is case-sensitive.
+
+Age = 20
+age = 18
+
+# 11. Identify the invalid variable name:
+# Answer:
+# 1name → Invalid
+# name_1 → Valid
+# _name → Valid
+# name$ → Invalid
+
+# 12. Write variables to store:
+age = 18
+salary = 5000
+city = "Hyderabad"
+
+# 13. What happens if you assign new value to same variable?
+# Answer: The old value gets overwritten by the new value.
+
+x = 10
+x = 20
+
+# 14. Can we use Python keywords as variable names?
+# Answer: No
